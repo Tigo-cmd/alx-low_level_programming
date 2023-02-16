96e7545a549b32b6c4b521e3c1c7ee0d481ea8af
EmmanuelTigo <ja16162040@gmail.com>
2023-02-16 17:15:58 -0500

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+* thi program prints with put function
+* 
+*/
+int main(void)
+{
+  
+int main(void)
+{
+	  puts("Programming is like building a multilingual puzzle \n");
+	  return (0);
+}
