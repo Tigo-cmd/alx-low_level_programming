3a17bf6dba5f89fe01fdfe11af9b1656c4efca90
EmmanuelTigo <ja16162040@gmail.com>
2023-02-16 17:21:04 -0500

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * this program prints with printf function.
+*/
+
+int main(void)
+{
+	  printf("with proper grammar, but the outcome is a piece of art,\n");
+	  return (0);
+}
