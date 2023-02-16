eaab319ff6aa7c06e1671ce2173e479c5faf4bac
Tigo-cmd <ja16162040@gmail.com>
2023-02-16 15:11:50 -0800

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Description: A C program that prints with printf function.
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-  /*prints output with puts function*/
-   puts("\"Programming is like building a multilingual puzzle \n");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
