3c83218dd50d469b6a50ce305614ab14de2697b9
Tigo-cmd <ja16162040@gmail.com>
2023-02-17 12:58:10 -0800

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /**
 * main - Enrty point
@@ -16,7 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is negative\n", n);
+	        printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
