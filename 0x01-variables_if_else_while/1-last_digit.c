c90275aec60000ca615d80b2f5a65945c6a37f2e
Tigo-cmd <ja16162040@gmail.com>
2023-02-18 01:04:58 -0800

    new

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+* main - Entry point
+*
+* Description - this programs checkes wheather a number
+* is positive or negative
+*
+* Return - Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+	int i;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	i = n % 10;
+	
+	/* if statement */
+	if (i > 5)
+		printf("last digit of %d is %d and is greater than 5\n", n, i);
+	else if (i == 0)
+		printf("the string is 0\n");
+	else if (i < 6)
+		printf("last digit of %d is %d and is");
+		printf("less than 6 ande not 0\n", n, i);
+}
