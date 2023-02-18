ff8a0c5011f15ab22d2f7aa406205ac0a1b0f640
Tigo-cmd <ja16162040@gmail.com>
2023-02-18 00:19:56 -0800

    sohard

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
+
 /**
-* main - Enrty point
-*
-* Description: this program prints wheather a
-* random number is positive or negative
-*
-* Return: Always 0 (Sucsess)
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-		printf("%d is positive\n", n);
-        else if (n == 0)
-		printf("%d is zero\n", n);
-        else if (n < 0)
-		printf("%d is negative\n", n);
-        else
-		printf("strand of int\n");
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/*my statement*/
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+
+	return (0);
 }
