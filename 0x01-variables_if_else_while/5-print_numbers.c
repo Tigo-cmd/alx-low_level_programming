5bd8eeab937e04f3c4daa23fe36a3a5d660b22af
Tigo-cmd <ja16162040@gmail.com>
2023-02-18 01:53:16 -0800

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description - this program prints all single digits
+* numbers of 10 starting from 0
+*
+* Return - Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	
+	return (0);
+}
