adf0d02139d973985fbb3a5c2dd1071c7a828974
Tigo-cmd <ja16162040@gmail.com>
2023-02-19 09:09:09 -0800

    new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description - this programs checkes wheather a number
+* is positive or negative
+*
+* Return - Always 0 (success)
+*/
+
+int main(void)
+{
+	/* my statement */
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		/* code */
+		putchar(i);
+	}
+	putchar('\n');
+
+	return(0);
+}
