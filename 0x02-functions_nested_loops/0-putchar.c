bc2a0e101bb42e0766708e22c7a3a5a59b000aec
EmmanuelTigo <ja16162040@gmail.com>
2023-02-21 07:39:00 -0500

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+* main- this is  the main function
+*
+* Decscription: this program prints "_putchar"
+*
+* Return: Always (0) success
+*/
+
+int main(void)
+{
+	/*my code comes here*/
+	printf("_putchar\n");
+
+	return(0);
+}
