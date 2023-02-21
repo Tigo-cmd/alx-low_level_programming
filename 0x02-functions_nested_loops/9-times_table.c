db4393c742f16a71f7898bc000f7361424b3e47b
EmmanuelTigo <ja16162040@gmail.com>
2023-02-21 17:49:39 -0500

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
- * main - check the code
+ * times_table - prints the 9 times table
  *
- * Return: Always 0.
- */
-int main(void)
-{
-    times_table();
-    return (0);
-}
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
+ *
+*/
+
 void times_table(void)
 {
 	int num, mult, prod;
