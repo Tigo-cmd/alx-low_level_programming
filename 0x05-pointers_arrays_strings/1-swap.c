e5e53789568ce69abd4f4ee81ce1df973813f297
Tigo-cmd <ja16162040@gmail.com>
2023-02-28 00:49:06 -0800

    growing

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+* swap_int -  function that swaps the values of two integers.
+* @a: first int parameter
+* @b: second int parameter
+* Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+    *a = 42;
+    *b = 98;
+}
