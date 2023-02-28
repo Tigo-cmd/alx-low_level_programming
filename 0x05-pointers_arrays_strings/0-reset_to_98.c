fcf76292a97ab2a21b044bcfd98bdf2428f0ef37
Tigo-cmd <ja16162040@gmail.com>
2023-02-28 00:39:53 -0800

    nawa o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
-#include"main.h"
-
 /**
-* Description: takes a pointer to an int as parameter and
-* updates the value it points to to 98
-*
-* @n: pointer input
-*/
+ * reset_to_98 - takes a pointer to an int parameter
+ *               and updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
 
 void reset_to_98(int *n)
 {
-   *n = 98;
+	*n = 98;
 }
