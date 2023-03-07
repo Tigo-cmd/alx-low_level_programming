1b5c6d734abd8a43824f0547034c99cda039c594
Tigo-cmd <ja16162040@gmail.com>
2023-03-07 02:24:13 -0800

    nice fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area.
- * @src: memory source to be copied from
- * @dest: memory destination of copied char
- * @n: amount of bytes to be copied from src to dest
- * Return: updated dest
- */
+* _memcpy - copies memory area.
+* @src: memory source to be copied from
+* @dest: memory destination of copied char
+* @n: amount of bytes to be copied from src to dest
+* Return: updated des
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
   unsigned int i;
