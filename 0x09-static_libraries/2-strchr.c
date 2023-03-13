264e13e345ca987332f8c0149c59afbf6ff94bca
EmmanuelTigo <ja16162040@gmail.com>
2023-03-12 22:13:45 -0400

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,36 +1,27 @@
 /**
- * _strncpy - A function that copies a string.
+ * _strchr - a function that locates a character in a string
  *
- * @dest: pointer to destination input buffer
- * @src: pointer to source input buffer
- * @n: bytes of @src
+ * @s: pointer to our string array input
+ * @c: character to locate from input array
  *
- * Return: @dest
+ * Return: first occurence of charatcer or null if not found
 */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strchr(char *s, char c)
 {
-	int i;
-
-	/**
-	 * iterate through src array
-	 * where if there is no null byte
-	 * among the first n bytes of source
-	 * the string placed in dest will not be
-	 * null terminated
-	*/
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	/**
-	 * if the length of source is less than n
-	 * write additional nullbytes to dest to
-	 * ensure that a total of n bytes is written
-	*/
-	while (i < n)
+	while (*s != '\0')
 	{
-		dest[i] = '\0';
-		i++;
+		if (*s == c)
+			return (s);
+		s++;
 	}
-
-	return (dest);
+	/**
+	 * if c is '\0', you should return
+	 * the pointer to the '\0' of the
+	 * string s
+	*/
+	if (*s == c)
+		return (s);
+	/*return null if not found*/
+	return ('\0');
 }
