9614c187082eb7f0f7b40ab2428c644867827f45
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 19:27:44 -0400

    updated

--- 0x00-hello_world/filecreate.sh
+++ 0x00-hello_world/filecreate.sh
@@ -3,6 +3,19 @@ echo "file >"
 read file
 touch $file
 echo "#include <stdio.h>" > $file
+echo "/**
+* main - Entry point
+*
+* Description: A program that prints the size of
+* various types on the computer it is compiled and run on.
+*
+* Return: 0(success)
+*/
+
+int main(void)
+{
+	return (0);		
+}" >> $file
 chmod 764 $file
 bash dirgit.sh
 gedit $file
