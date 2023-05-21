63ea8d4291df44b3a255866d3393df91ef97ac91
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 18:35:49 -0400

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-/**
+/*
+*
 * main: Entry point
 *
-* Decription: prints formatted strings using puts function
+* Description: prints "Programming is like
+* building a multilingual puzzle using puts function
 *
 * Return: Always 0(sucess)
 */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+
+	return (0);
 }
