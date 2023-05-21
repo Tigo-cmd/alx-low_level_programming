6698b52e6690f8f973f41bbe310fd291cee1b0f0
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 18:31:51 -0400

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+* main: Entry point
+*
+* Decription: prints formatted strings using puts function
+*
+* Return: Always 0(sucess)
+*/
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
--- 0x00-hello_world/filecreate.sh
+++ 0x00-hello_world/filecreate.sh
@@ -2,9 +2,7 @@
 echo "file >"
 read file
 touch $file
-echo "#!/bin/bash" > $file
-echo "cnt >"
-read cnt
-echo $cnt >> $file
+echo "#include <stdio.>" > $file
 chmod 764 $file
 bash dirgit.sh
+gedit $file
