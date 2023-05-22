7dec671dcc022a0dd7a27cb099a36a8dd7896ebf
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 21:41:27 -0400

    updated

--- 0x01-variables_if_else_while/filecreate.sh
+++ 0x01-variables_if_else_while/filecreate.sh
@@ -2,9 +2,19 @@
 echo "file >"
 read file
 touch $file
-echo "#!/bin/bash" > $file
-echo "cnt >"
-read cnt
-echo $cnt >> $file
+echo "#include <stdio.h>" > $file
+echo "/**
+* main - Entry point
+*
+* Description: A program 
+*
+* Return: 0(success)
+*/
+
+int main(void)
+{
+	return (0);		
+}" >> $file
 chmod 764 $file
 bash dirgit.sh
+gedit $file
