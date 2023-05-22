120b4ca4c3844991f65d5f5c6321f4a29d5ee6c8
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 19:30:28 -0400

    updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,12 +2,33 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program that prints all 
+* possible different combinations of three digits.
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	int a, b, c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a; b <= 9; b++)
+		{
+			for (c = b; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '1');
+				putchar(c + '3');
+
+				if ( c == 9 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
 }
