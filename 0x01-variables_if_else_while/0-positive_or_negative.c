838e36dc8f05939d6d8ab90f53cd10185abb796e
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 21:50:13 -0400

    updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,23 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program that indicates a positive or negative num
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d\n", "is positive");
+	if (n < 0)
+		printf("%d\n", "is negative");
+	else
+		printf("%d\n", "is zero");
+	return (0);
 }
