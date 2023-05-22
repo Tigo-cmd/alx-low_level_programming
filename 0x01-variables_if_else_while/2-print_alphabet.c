fe7c65083de9414285e937ba44d72fe578fd8d00
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 11:59:51 -0400

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char b;
 
-	for (b = 'a'; b < 'z'; b++)
+	for (b = 'a'; b <= 'z'; b++)
 	{
 		putchar(b);
 	}
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	char b;
 
-	for (b = 'a'; b < 'z'; b++)
+	for (b = 'a'; b <= 'z'; b++)
 	{
 		putchar(b);
 	}
-	for (b = 'A'; b < 'Z'; b++)
+	for (b = 'A'; b <= 'Z'; b++)
 	{
 		putchar(b);
 	}
