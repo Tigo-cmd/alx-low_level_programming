94b0db4e443d97cbbf5978148a13d2c6d50fe91c
Tigo-cmd <ja16162040@gmail.com>
2023-05-21 20:12:44 -0400

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,21 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program to print using putchar function
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	char wrd[58] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	int i;
+
+
+	for (i = 0; i < wrd; i++)
+	{
+		putchar(wrd[i]);
+	}
+	return (0);
 }
