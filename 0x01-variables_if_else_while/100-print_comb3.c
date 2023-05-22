cee7f3e87db0a4065ae413f87bffdc8e6cf897f0
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 18:34:03 -0400

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,12 +2,28 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program possible combinations of two digits
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a == 9 || a != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
 }
