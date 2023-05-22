ec5292362db705b6129168fc01e33791c7b696ee
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 15:52:55 -0400

    updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,12 +2,25 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program print the 16 hexadcimal figures
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	int a = 0;
+	char c;
+
+	while (a <= 9)
+	{
+		putchar(a + '0')
+		a++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
