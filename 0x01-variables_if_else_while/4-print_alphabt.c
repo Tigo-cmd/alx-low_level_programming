f5f7c34b69d2e781a89309b52f284372b8e17972
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 12:50:46 -0400

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,12 +2,24 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program that prints the alphabet
+* in lowercase, followed by a new line.
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+		if (b == 'd')
+			a = 'e';
+		if (b == 'p')
+			a = 'q';
+	}
+	putchar('\n');
+	return (0);
 }
