298cacb3a45c8132ac8b10815bb7cf76207999dd
Tigo-cmd <ja16162040@gmail.com>
2023-05-22 11:18:19 -0400

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program thats prints the last digit of a integer.
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
-	return (0);		
+	int n, lastdigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lastdigit = n % 10;
+	if (lastdigit > 5)
+		printf("last digit of %d is %d and is less ",
+			"than 6 and not 0\n", n, lastdigit);
+	if (lastdigit == 0)
+		printf("Last digit of %d is 0 and is 0\n", n);
+	if (lastdigit < 6 && lastdigit != 0)
+		printf("Last digit of %d is %d and is less ",
+			"than 6 and not 0\n", n, lastdigit);
+	return (0);
 }
