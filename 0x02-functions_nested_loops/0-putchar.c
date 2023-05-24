2bf1df9715ed9fbc7fa9b8cc14f44cb2e077b0c3
Tigo-cmd <ja16162040@gmail.com>
2023-05-24 10:39:34 -0400

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,22 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program prints "_putchar" followed
+* by a new line
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
+	char f[9] = "_putchar"
+	int i;
+
+	while (i < 9)
+	{
+		_putchar(f);
+		i++;
+	}
+	_putchar('\n')
 	return (0);		
 }
