0e6c2521ed0a0f55b51f4021c3d84103bb2c6b87
Tigo-cmd <ja16162040@gmail.com>
2023-05-24 17:44:16 -0400

    updated

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
-* 6-abs.c - 
-* @param
-* @param
+* _abs - prints absolute integer
+* @c: first parameter
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription:  function that computes the absolute value of an integer.
+*
+* Return: Always(int) success
 */
-int _abs(int);
+int _abs(int c)
 {
-	
-} 
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+	else if (c > 0)
+	{
+		c = c;
+	}
+}
