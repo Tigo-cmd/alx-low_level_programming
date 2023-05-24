38555a7708c9d4499a72e61840f4fc4a6f185db9
Tigo-cmd <ja16162040@gmail.com>
2023-05-24 13:48:13 -0400

    updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
-* 3-islower.c - 
-* @param
-* @param
+* _islower - checks for lowercase alphabeths
+* @c: first operand
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription:  function that checks for lowercase character
+*
+* Return: returns 1 if lowercase and 0 if othwerwise
 */
-int _islower(int c);
+int _islower(int c)
 {
-	
-} 
+	if (c >= 122 || c < 97)
+		return (0);
+	else
+		return (1);
+
+}
