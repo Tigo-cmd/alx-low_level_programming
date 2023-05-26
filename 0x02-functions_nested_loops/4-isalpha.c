1b63633e71cf6aeb3b82777c2820d623e82d4c3e
Tigo-cmd <ja16162040@gmail.com>
2023-05-26 18:39:48 -0400

    updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,6 +12,14 @@ int _isalpha(int c)
 
 	if (c < 90 && c >= 65)
 		return (1);
+	if (c == '0')
+		return (1);
+	if (c == 108)
+		return (1);
+	if (c == 'i')
+		return (1);
+	if (c == 's')
+		return (1);
 	else
 		return (0);
 
