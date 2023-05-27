6553fe2137279a194115dbca21ad794135833cc4
Tigo-cmd <ja16162040@gmail.com>
2023-05-26 20:42:17 -0400

    updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,25 @@
 #include "main.h"
 /**
-* 9-times_table.c - 
-* @param
-* @param
+* times_table - prints the 9 times tabile starting with 0
 *
-* Decsription: 
+* Decsription: function that prints the 9 times table, starting with 0
 * Return: Always(0) success
 */
 void times_table(void);
 {
-	
-} 
+	int i, j, num;
+
+	num = 9;
+	for (i = 0; i <= num; ++i)
+	{
+		for (j = 0; j <= num; j++)
+		{
+			printf("%d", i * j)
+			if (j < num)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
