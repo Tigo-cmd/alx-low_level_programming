566abcd6e6165e6654b68fd7c0f2dc0a4d6cbfab
Tigo-cmd <ja16162040@gmail.com>
2023-05-29 04:32:23 -0400

    updated

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 11-print_to_98.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void print_to_98(int n);
+{
+	
+} 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ int _abs(int c);
 int print_last_digit(int);
 int add(int, int);
 void times_table(void);
+void print_to_98(int n);
 #endif
