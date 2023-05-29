70ea1aa6b62b9e9bf3d509105cb9f1203b80e7e6
Tigo-cmd <ja16162040@gmail.com>
2023-05-29 04:38:59 -0400

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,31 @@
 #include "main.h"
 /**
-* 11-print_to_98.c - 
-* @param
-* @param
+* print_to_98 - prints all natrual numbers
+* @n: first parameter
 *
-* Decsription: 
+* Decsription: function that prints all natural
+* numbers from n to 98, followed by a new line.
 * Return: Always(0) success
 */
-void print_to_98(int n);
+void print_to_98(int n)
 {
-	
-} 
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	if (n == 98)
+		printf("%d", 98);
+	return (0);
+}
