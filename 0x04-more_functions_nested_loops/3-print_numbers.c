e0c17f258d09a73cfe173e9eb121530a23040c26
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 15:13:48 -0400

    updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
-* 3-print_numbers.c - 
-* @param
-* @param
+* print_numbers - prints numbers from 0 t0 9
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints the numbers,
+* from 0 to 9, followed by a new line.
+* Return: nothing
 */
-void print_numbers(void);
+void print_numbers(void)
 {
-	
-} 
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
