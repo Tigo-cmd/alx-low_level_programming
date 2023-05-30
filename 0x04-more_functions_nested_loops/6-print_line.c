ad5d787ee2c70e05a87acaca3a73b4ece2589527
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 17:11:59 -0400

    updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
-* 6-print_line.c - 
-* @param
-* @param
+* print_line - draws a straight line
+* @n: first parameter
 *
-* Decsription: 
+* Decsription: function that draws a straight line in the terminal.
 * Return: Always(0) success
 */
-void print_line(int n);
+void print_line(int n)
 {
-	
-} 
+	int b;
+
+	for (b = 0; b <= n; b++)
+	{
+		if (n == 0 && n < 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
+}
