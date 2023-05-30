11d244f5f375faf91924d6fc4e781b65375d3296
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 14:42:44 -0400

    updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,15 @@
 #include "main.h"
 /**
-* isupper - checks for upper case characters
+* _isupper - checks for upper case characters
 * @c: first parameter
 *
 * Decsription: function that checks for uppercase character.
 * Return: Always(0) success
 */
-int _isupper(int c);
+int _isupper(int c)
 {
-	char b
-	c = 0;
-	if b <= 90 || b > 65)
-	{
-		c = 1
+	if (b <= 90 || b > 65)
 		return (1);
-	}
 	else
 		return (0);
 }
