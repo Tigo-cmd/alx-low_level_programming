c0ea5a8dfd1b5e7e857212c52de8e153e37db54b
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 14:59:06 -0400

    updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-* 1-isdigit.c - 
-* @param
-* @param
+* _isdigit - checks for a digit (0 through 1)
+* @c: first integer
 *
-* Decsription: 
+* Decsription: function that checks for a digit (0 through 9)
 * Return: Always(0) success
 */
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	
-} 
+	if (c <= 57 && c >= 48)
+		return (1);
+	else
+		return (0);
+}
