b70620afb3608a1ef55d9814e22f3e7878ae4764
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 15:32:48 -0400

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,22 @@
 #include "main.h"
 /**
-* 5-more_numbers.c - 
-* @param
-* @param
+* more_numbers - prints 10 times the numbers, from 0 to 14
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints 10 times the numbers,
+* from 0 to 14, followed by a new line.
+* Return: nothing
 */
-void more_numbers(void);
+void more_numbers(void)
 {
-	
-} 
+	int c, d;
+
+	d = 0;
+	while (d < 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + '0');
+		}
+		d++;
+	}
+}
