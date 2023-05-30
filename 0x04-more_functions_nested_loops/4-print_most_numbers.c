2a7a506b4cd7e1374c0ac176e2c6f4c22e3ae095
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 15:25:20 -0400

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
-* 4-print_most_numbers.c - 
-* @param
-* @param
+* print_most_numbers - prints numbers from 0 to 9
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints the numbers,
+* from 0 to 9, followed by a new line.
+* Return: nothing
 */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-	
-} 
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+		if (c == 1 || c == 3)
+			c = c + 1;
+	}
+	_putchar('\n');
+}
