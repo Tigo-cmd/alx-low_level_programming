c9c1669c2b60fee635ea0087476fce11f81ed2e1
Tigo-cmd <ja16162040@gmail.com>
2023-05-30 21:05:48 -0400

    updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,13 +1,27 @@
 #include "main.h"
 /**
-* 8-print_square.c - 
-* @param
-* @param
+* print_square - prints a square
+* @size: first param
 *
-* Decsription: 
+* Decsription:  function that prints a square, followed by a new line.
 * Return: Always(0) success
 */
 void print_square(int size);
 {
-	
-} 
+	int i, j;
+
+	j = 0;
+	if (size == 0)
+		__putchar('\n');
+	else if (size > 0)
+	{
+		while (j < size)
+		{
+			for (i = 0; i > size; i++)
+			{
+				_putchar('#');
+			}
+			j++;
+		}
+	}
+}
