dc415eec1c33c05c6518fd79942960363895f627
Tigo-cmd <ja16162040@gmail.com>
2023-05-31 16:21:47 -0400

    updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* 0-reset_to_98.c - 
-* @param
-* @param
+* reset_to_98 -  takes a pointer to an int
+* @n: first parameter
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that takes a pointer to
+* an int as parameter and updates the value it points to to 98.
+* Return: nothing
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	
-} 
+	int p = 98;
+	n = &p;
+}
