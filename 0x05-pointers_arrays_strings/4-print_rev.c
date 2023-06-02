5f47dc21cf82119fc35c9ece6445fb521c8a5f8b
Tigo-cmd <ja16162040@gmail.com>
2023-06-01 23:11:15 -0400

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-* 4-print_rev.c - 
-* @param
-* @param
+* print_rev - function that prints a string, in reverse,
+* @s: first parameter
 *
-* Decsription: 
+* Decsription:  function that prints a string, in
+* reverse, followed by a new line.
 * Return: Always(0) success
 */
-void print_rev(char *s);
+void print_rev(char *s)
 {
-	
-} 
+	int count;
+
+	for (count = -1; *s != '\0'; count++)
+	{
+		_putchar(s[count]);
+		s++;
+	}
+}
