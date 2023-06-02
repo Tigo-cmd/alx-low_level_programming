f7364386f7c27a206e8163328d1a6a5ba1b9a71f
Tigo-cmd <ja16162040@gmail.com>
2023-06-02 01:25:22 -0400

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,36 @@
 #include "main.h"
 /**
-* 7-puts_half.c - 
-* @param
-* @param
+* puts_half - prints half of a string
+* @str: pointer to array
 *
-* Decsription: 
+* Decsription:  function that prints half of a string, followed by a new line.
 * Return: Always(0) success
 */
 void puts_half(char *str);
 {
-	
-} 
+	int b = 0, mod;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	mod = b % 2;
+	if (mod == 0)
+	{
+		b = b / 2;
+		while (str[b] != '\0')
+		{
+			_putchar(str[b]);
+			b++;
+		}
+	}
+	else if (mod != 0)
+	{
+		b = (b - 1) / 2;
+		while (str[b] != '\0')
+		{
+			_putchar(str[b])
+			b++;
+		}
+	}
+}
