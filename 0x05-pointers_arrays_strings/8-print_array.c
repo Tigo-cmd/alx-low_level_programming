297e280e6cd906926905684612228b5328da226b
Tigo-cmd <ja16162040@gmail.com>
2023-06-04 16:16:56 -0400

    updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,11 +4,20 @@
 * @a: first parameter
 * @n: second parameter
 *
-* Decsription: function that prints n elements of 
+* Decsription: function that prints n elements of
 * an array of integers, followed by a new line.
 * Return: Always(0) success
 */
 void print_array(int *a, int n);
 {
-	
-} 
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		if (i == n - 1)
+			printf("%d", a[i]);
+	}
+	_putchar('\n');
+}
