5bf45847e85c8009bbb5d37c88ada80f2356a775
Tigo-cmd <ja16162040@gmail.com>
2023-06-05 11:50:02 +0100

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,22 @@
+
 #include "main.h"
 /**
-* 0-memset.c - 
-* @param
-* @param
+* _memset - prints a constant byte
+* @s: first pointer
+* @b: second parameter
+* @n: third parameter
 *
-* Decsription: 
+* Decsription: function that fills memory with a constant byte.
 * Return: Always(0) success
 */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	
-} 
+	unsigned int p = 0;
+
+	while (p < n)
+	{
+		s[p] = b;
+		p++;
+	}
+	return (s);
+}
