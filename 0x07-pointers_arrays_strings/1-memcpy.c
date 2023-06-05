7f639bc894b64b32270fca06a135440a9152cd8d
Tigo-cmd <ja16162040@gmail.com>
2023-06-05 12:28:06 +0100

    updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,22 @@
 #include "main.h"
 /**
-* 1-memcpy.c - 
-* @param
-* @param
+* _memcpy - copies memory data
+* @dest: first param
+* @src: second param
+* @n: last param
 *
-* Decsription: 
+* Decsription: function that copies memory area.
 * Return: Always(0) success
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	
-} 
+	char *ptr_dest = dest;
+
+	for (;; n--)
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+	}
+	return (ptr_dest);
+}
