34e6f66b6c1fada3834873b865044a936bbf3bd4
Tigo-cmd <ja16162040@gmail.com>
2023-06-05 19:20:41 +0100

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,32 @@
 #include "main.h"
 /**
-* 3-strspn.c - 
-* @param
-* @param
+* _strspn - prints prefix of a substring
+* @s: first param
+* @accept: second param
 *
-* Decsription: 
+* Decsription: function that prints prefix of a string
 * Return: Always(0) success
 */
-char *_strpbrk(char *s, char *accept);
+char *_strspn(char *s, char *accept)
 {
-	
-} 
+	unsigned int i = 0;
+	int j;
+
+	do {
+		j = 0;
+		char *p = accept;
+		do {
+			if (*s == *p)
+			{
+				j = 1;
+				break;
+			}
+			p++;
+		}while (*p);
+		if (!j)
+			break;
+		i++;
+		s++;
+	}while (*s);
+	return i;
+}
