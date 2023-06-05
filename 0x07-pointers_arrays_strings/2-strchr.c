0aa6c3306643f465adbaeb631db5dbb589ad5c16
Tigo-cmd <ja16162040@gmail.com>
2023-06-05 16:07:35 +0100

    updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
-* 2-strchr.c - 
-* @param
-* @param
+* _strchr - locates char in a string
+* @s: first parameter
+* @c: second parameter
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that locates a character in a string.
+* Return: NULL
 */
 char *_strchr(char *s, char c);
 {
-	
-} 
+	for (; s != '\0'; s++)
+	{
+		if (*s == '\0')
+			return (s);
+		if (c == '\0')
+			rerurn (s);
+	}
+	return NULL;
+}
