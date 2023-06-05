2100629c96f44db952a89f9d81fbc5e23cc6a90d
Tigo-cmd <ja16162040@gmail.com>
2023-06-05 15:17:14 -0400

    updated

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,23 @@
 #include "main.h"
 /**
-* 4-strpbrk.c - 
-* @param
-* @param
+* _strpbrk -  searches a string for any of a set of bytes.
+* @s: first param
+* @accept: firstparam
 *
-* Decsription: 
+* Decsription: function that searches a string for any of a set of bytes.
 * Return: Always(0) success
 */
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
 {
-	
-} 
+	do {
+		char *p = accept;
+
+		do {
+			if (*a == *s)
+				return (s);
+			p++;
+		} while (*p);
+		s++;
+	} while (*s);
+	return (NULL);
+}
