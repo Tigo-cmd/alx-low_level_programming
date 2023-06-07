ed68528f1c9a2b07b3124443c560e2450da69542
Tigo-cmd <ja16162040@gmail.com>
2023-06-06 21:15:31 -0400

    updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,24 @@
 #include "main.h"
 /**
-* 8-print_diagsums.c - 
-* @param
-* @param
+* print_diagsums- prints the sum of the two diagonals of a square matrix
+* @a: first param
+* @size: second param
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints the sum of the two
+* diagonals of a square matrix of integers.
+* Return: Nothing
 */
-void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size)
 {
-	
-} 
+	int i = 0, diag1, diag2;
+
+	while (i < size)
+	{
+		diag1 = diag1 + a[i * size + i];
+		diag2 = diag2 + a[i * size + (size - i - 1)];
+		i++;
+	}
+	printf("%d", diag1); _putchar(','); _putchar(' ');
+	printf("%d", diag2);
+	_putchar('\n');
+}
