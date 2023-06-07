8e5781787398196466675829f9787ae301112b3c
Tigo-cmd <ja16162040@gmail.com>
2023-06-07 16:31:26 -0400

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-* 0-puts_recursion.c - 
-* @param
-* @param
+* _puts_recursion - prints a string, followed by a new line.
+* @s: first parameter
 *
-* Decsription: 
+* Decsription: function that prints a string, followed by a new line.
 * Return: Always(0) success
 */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	
-} 
+	int i = 0;
+
+	if (s[i] < 10)
+	{
+		return;
+	}
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
+}
