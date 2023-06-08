d9c6191b333436b10371c8cdd06dd07d5bb6d9ce
Tigo-cmd <ja16162040@gmail.com>
2023-06-08 14:12:58 +0100

    updated

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-* 4-pow_recursion.c - 
-* @param
-* @param
+* _pow_recursion - returns the value of x
+* raised to the power of y
+* @x: first no
+* @y: second no
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that returns the value of x
+* raised to the power of y.
+* Return: the output.
 */
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y)
 {
-	
-} 
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1):
+	return (x * _pow_recursion(x, y -1));
+}
