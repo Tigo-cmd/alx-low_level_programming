a861ff3280601032abd595ccc075c783ff71dfad
Tigo-cmd <ja16162040@gmail.com>
2023-06-08 13:02:41 +0100

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,10 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	return (i);
+	i++;
+	return (i) + _strlen_recursion(s + 1);
 }
