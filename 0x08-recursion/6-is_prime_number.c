23663dc75bb3c023a831a4ff48e3d217e1565c0d
Tigo-cmd <ja16162040@gmail.com>
2023-06-08 17:58:45 -0400

    updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,40 @@
 #include "main.h"
 /**
-* 6-is_prime_number.c - 
-* @param
-* @param
+* _prime - determimes the divisor of numbers
+* @p: param
+* @div: divisor
 *
-* Decsription: 
+* Decsription: function Number is divisible by a
+*  factor other than 1 and itself
 * Return: Always(0) success
+*/
+int _prime(int p, int div);
+{
+	int b = div - 1;
+
+	if (div == 0)
+		return (1);
+	if (div != p)
+		return (1);
+	if (div  == 1)
+		return (0);
+
+	return (_prime(p, b));
+}
+/**
+* is_prime_number - input integer is a prime number, otherwise return 0;
+* @n: first param
+*
+* Decsription:  function that returns 1 if the input integer
+*  is a prime number, otherwise return 0.
+* Return: n
 */
 int is_prime_number(int n);
 {
-	
-} 
+	int din = n - 1;
+
+	if (n <= 1)
+		return (0);
+
+	return (!_prime(n, n - 1));
+}
