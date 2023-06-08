8fee6fabbbfb6c25a27c9376c5f6838472f530d6
Tigo-cmd <ja16162040@gmail.com>
2023-06-08 17:18:45 -0400

    updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,35 @@
 #include "main.h"
 /**
-* 5-sqrt_recursion.c - 
-* @param
-* @param
-*
-* Decsription: 
-* Return: Always(0) success
+* _func - initalizes sqrt acts as a helper
+* @p: func first param
+* @i: runner param
+* Decsription: functions that returns the natural square root of a number.
+* Return: helper recursion
 */
-int _sqrt_recursion(int n);
+int _func(int p, int i)
 {
-	
-} 
+	int mul;
+
+	mul = i * i;
+	if (mul == p)
+		return (i);
+	if (mul > p)
+		return (-1);
+	else
+		return (_func(p, i + 1));
+/**
+* _sqrt_recursion - returns the natural square root of a number.
+* @n: parameter
+* Decsription: functions that returns the natural square root of a number.
+* Return: sqrt of number
+*/
+int _sqrt_recursion(int n)
+{
+	int b = 1;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_func(p, b));
+}
