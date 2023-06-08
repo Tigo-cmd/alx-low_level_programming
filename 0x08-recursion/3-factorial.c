1ed89d6ebb9c4c7f73f52a2a2600d0e21216363a
Tigo-cmd <ja16162040@gmail.com>
2023-06-08 13:28:09 +0100

    updated

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
-* 3-factorial.c - 
-* @param
-* @param
+* _factorial - prints factorial of a number
+* @n: first parameter
 *
-* Decsription: 
+* Decsription:  function that returns the
+* factorial of a given number
 * Return: Always(0) success
 */
-int factorial(int n);
+int factorial(int n)
 {
-	
-} 
+	if (n <  0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
