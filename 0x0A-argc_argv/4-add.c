543716bd73802584f0f696a41d56ba62ae0f1039
Tigo-cmd <ja16162040@gmail.com>
2023-06-12 07:45:15 -0400

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
 * main - Entry point
-*
-* Description: A program 
+* @argc: counter
+* @argv: pointer array
+* Description: A program that adds positive numbers.
 *
 * Return: 0(success)
 */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	return (0);		
+	int argc_Count, fin = 0;
+
+	if (argc <= 1)
+		printf("0\n");
+	for (argc_count = 1; argc_Count < argc; argc_count++)
+	{
+		int array_Count = 0;
+
+		while (argv[argc_Count][array_Count] != '\0')
+		{
+			if (isalpha(argv[argc_Count][array_Count])
+			{
+				printf("Error\n");
+				return (1);
+			}
+			array_Count++;
+		}
+		fin = fin + atoi(argv[argc_Count]);
+	}
+	if (fin == 0)
+		return (0);
+	printf("%d\n", fin);
+	return (0);
 }
