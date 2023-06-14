944f4af863478041efffc365665fa7213bbb7891
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 11:59:42 -0400

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
 /**
-* 0-create_array.c - 
-* @param
-* @param
+* create_array - creates an array of chars
+* @size: the size of the memory to print
+* @c: array to print
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that creates an array of chars,
+*  and initializes it with a specific char.
+* Return: a pointer to the array, or NULL if it fails
 */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-	
-} 
+	unsigned int b = 0;
+
+	if (size == 0)
+		return (NULL);
+	char *a = (char *)malloc(size * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+	while (b < size)
+	{
+		a[b] = c;
+		b++;
+	}
+	return (a);
+}
