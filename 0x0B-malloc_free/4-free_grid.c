904bbb8bf47028b612ae31e55dc1bf07e0683090
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 19:26:25 -0400

    updated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,25 @@
 #include "main.h"
 /**
-* 4-free_grid.c - 
-* @param
-* @param
+* free_grid- function that frees a 2 dimensional grid previously created
+* @grid: first param
+* @height: second param
 *
-* Decsription: 
+* Decsription: unction that frees a 2 dimensional
+* grid previously created by your alloc_grid function.
 * Return: Always(0) success
 */
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
 {
-	
-} 
+	int i = 0;
+
+	if (grid == NULL)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
