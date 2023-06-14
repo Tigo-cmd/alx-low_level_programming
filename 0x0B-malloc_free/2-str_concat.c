d05aa6a6e328ecdee3f05de78d854d4a19d0ae6c
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 17:55:26 -0400

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int a, b, i, j, sum;
 
-	if (s1 == NULL || s2 == NULL)
-		return ('\0');
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
 	{
