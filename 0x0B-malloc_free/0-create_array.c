c8c407e906316963ebdecee632db827d4df6c81e
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 12:03:59 -0400

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int b = 0;
+	char *a;
 
 	if (size == 0)
 		return (NULL);
-	char *a = (char *)malloc(size * sizeof(char));
+	a = (char *)malloc(size * sizeof(char));
 
 	if (a == NULL)
 		return (NULL);
