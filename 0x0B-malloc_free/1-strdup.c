2b96ed5864e2fc4f698fa6d993ac6a3047ecbaa4
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 14:25:48 -0400

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,13 +1,30 @@
 #include "main.h"
 /**
-* 1-strdup.c - 
-* @param
-* @param
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: array string to be duplicated
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that returns a pointer to a newly allocated space in
+* memory, which contains a copy of the string given as a parameter.
+* Return: pointer of the new string
 */
 char *_strdup(char *str);
 {
-	
-} 
+	char *str2;
+	int i = 0, b = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	str2 = (char *)malloc(i * sizeof(char));
+	if (str2 == NULL)
+		return (NULL);
+	while (b <= i)
+	{
+		str2[b] = str[b];
+		b++;
+	}
+	return (str2);
+}
