2f9d772c035be65e4d11153a8fbb778f10acde02
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 17:30:48 -0400

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,13 +1,53 @@
 #include "main.h"
+#include <stdlib.h>
 /**
-* 2-str_concat.c - 
-* @param
-* @param
+* str_concat - concatenates two strings
+* @s1: first string to be concatenated
+* @s2: second srting to be concatenated
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that concatenates two strings
+* Return: pointer to the concatenated string
 */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
-	
-} 
+	char *p;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+	int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	int j = 0;
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	int sum = i + j;
+
+	p = (char *)malloc((sum * sizeof(char)) + 1);
+
+	if (p == NULL)
+		return (NULL);
+	int a = 0;
+
+	while (a < i)
+	{
+		p[a] = s1[a];
+		a++;
+	}
+	int b = 0;
+
+	while (b < j)
+	{
+		p[a + b] = s2[b];
+		b++;
+	}
+	p[i + j] = '\0';
+	return (p);
+}
