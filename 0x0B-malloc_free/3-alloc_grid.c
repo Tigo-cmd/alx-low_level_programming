4802a808a86de7aa2b0aa4320459d3a662da2209
Tigo-cmd <ja16162040@gmail.com>
2023-06-14 18:25:48 -0400

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,38 @@
 #include "main.h"
 /**
-* 3-alloc_grid.c - 
-* @param
-* @param
+* alloc_grid - returns a pointer to a 2 dimensional array of integers.
+* @width: first param
+* @height: second param
 *
-* Decsription: 
+* Decsription: function that returns a
+* pointer to a 2 dimensional array of integers
 * Return: Always(0) success
 */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
-	
-} 
+	char **p;
+	int i = 0, b = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = (int **)malloc(height * sizeof(int *));
+	if (p == NULL)
+		return (NULL);
+
+	while (i < height)
+	{
+		p[i] = (int *)malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			while (b < i)
+			{
+				free(p[b]);
+				b++;
+			}
+			free(p);
+			return (NULL);
+		}
+		i++;
+	}
+	return (p);
+}
