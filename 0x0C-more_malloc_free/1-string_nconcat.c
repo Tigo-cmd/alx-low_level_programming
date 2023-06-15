59926ffa42ace418ec346f52f439601de3e44ae0
Tigo-cmd <ja16162040@gmail.com>
2023-06-15 18:53:54 -0400

    updated

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 1-string_nconcat.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	
+} 
