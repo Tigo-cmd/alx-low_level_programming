f0af13dc7cbe4ad17245981ccd46b43708462e42
Tigo-cmd <ja16162040@gmail.com>
2023-06-15 19:20:20 -0400

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,51 @@
 #include "main.h"
 /**
-* 1-string_nconcat.c - 
-* @param
-* @param
-*
-* Decsription: 
-* Return: Always(0) success
+* string_nconcat - concatenates two strings.
+* @s1: first pointer to string
+* @s2: second pointer to string
+* @n: last parameter
+* Decsription: function that concatenates two strings.
+* Return: pointer to allocated memory and NULL if failed
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	
-} 
+	int a, b, i = 0, j = 0, sum;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (n < j)
+	{
+		j = n;
+		sum = j + i;
+		p = malloc((sum * sizeof(char)) + 1);
+	}
+	if (n >= j)
+	{
+		sum = j + i;
+		p = malloc((sum + 1) * sizeof(char));
+	}
+	if (p == NULL)
+		return (NULL);
+	for (a = 0; a < i; a++)
+	{
+		p[a] = s1[a];
+	}
+	for (b = 0; b < j; b++)
+	{
+		p[a + b] = s2[b];
+	}
+
+	p[i + j] = '\0';
+	return (p);
+}
