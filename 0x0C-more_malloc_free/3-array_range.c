592a5395e87ecf0e36f5ed476247a4b81b33f8c4
Tigo-cmd <ja16162040@gmail.com>
2023-06-16 16:52:48 -0400

    updated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,13 +1,27 @@
 #include "main.h"
 /**
-* 3-array_range.c - 
-* @param
-* @param
+* array_range - creates an array of integers.
+* @min: starter parameter
+* @max: count of the starter
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: Write a function that creates an array of integers.
+* Return: the pointer to the newly created array
 */
-int *array_range(int min, int max);
+int *array_range(int min, int max)
 {
-	
-} 
+	int i, *p, g = 0;
+
+	/*min = min + 1;*/
+	if (min < max)
+		return (NULL);
+	i = max - min + 1;
+	p = malloc(i * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	while (g < i)
+	{
+		p[g] = min + g;
+		g++;
+	}
+	return (p);
+}
