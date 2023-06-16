e327fd0515512840e635660d7f90494bdab83e20
Tigo-cmd <ja16162040@gmail.com>
2023-06-16 07:01:45 -0400

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,19 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *a;
+	char *a;
+	unsigned int p = nmemb * size, f = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
+	while (f < p)
+	{
+		a[f] = 0;
+		f++;
+	}
+	a = (void *)a;
 	return (a);
 }
