4284590b9e1726647d74dc091558eb03dafac2c2
Tigo-cmd <ja16162040@gmail.com>
2023-06-16 06:31:01 -0400

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
 /**
-* 2-calloc.c - 
-* @param
-* @param
+* _calloc - allocates memory for an array, using malloc.
+* @nmemb: array to be allocated
+* @size: size of byte
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that allocates memory for an array, using malloc.
+* Return: pointer to the allocated memory
 */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
-} 
+	void *a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc((nmemb + 1) * size);
+	if (a == NULL)
+		return (NULL);
+	return (a);
+}
