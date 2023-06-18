9952eb0795c75347780f3fd8e6c3a5a1d3f2cb76
Tigo-cmd <ja16162040@gmail.com>
2023-06-18 17:13:28 -0400

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,12 +2,25 @@
 /**
 * main - Entry point
 *
-* Description: A program 
+* Description: A program performs The “Fizz-Buzz test” 
 *
 * Return: 0(success)
 */
 
 int main(void)
 {
+	int p;
+
+	for (p = 0; p <= 100; p++)
+	{
+		if (p % 3 == 0 && p % 5 == 0 && p != 0)
+			printf("FizzBuzz ");
+		else if (p % 3 == 0 && p != 0)
+			printf("Fizz ");
+		else if (p % 5 == 0 && p != 0)
+			printf("Buzz ");
+		else
+			printf("%d ", p);
+	}
 	return (0);		
 }
