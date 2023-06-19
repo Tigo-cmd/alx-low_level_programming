8c00566a42eee7284237c93fae64bac464912010
Tigo-cmd <ja16162040@gmail.com>
2023-06-18 23:11:05 -0400

    updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
-* sub - returns length
-* @s: first param
-* @i: second param
-* @j: last param
-*
-* Decsription: helper function
-* Return: sub
-*/
-int sub(char *s, int i, int j)
-{
-	if (i <= j)
-		return (1);
-	if (s[i] != s[j])
-		return (0);
-	return (sub(s, i + 1, j - 1));
-}
-/**
 * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
 * @s: first param
 *
@@ -26,14 +9,14 @@ int sub(char *s, int i, int j)
 */
 int is_palindrome(char *s)
 {
-	int i = 0;
+	int i = 1;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	if (i <= 1)
+	if (*s == '\0')
 		return (1);
-	
-	return (sub(s, 0, i - 1));
+	if (*(s + i) == '\0')
+		return (1);
+	if (*s != *(s + i)
+		return (0);
+
+	return (is_palindrome(s + i));
 }
