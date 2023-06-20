81af3694e6352424fbd4c721ff467e3d04c1accd
Tigo-cmd <ja16162040@gmail.com>
2023-06-20 02:48:26 -0400

    updated

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
 * Decsription: function that initialize a variable of type struct dog
 * Return: Always(0) success
 */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
