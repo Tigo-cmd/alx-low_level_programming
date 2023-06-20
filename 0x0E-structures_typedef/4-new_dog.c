1786b6f41859bb4124d7cb4885a003b3c8a25467
Tigo-cmd <ja16162040@gmail.com>
2023-06-20 11:10:17 -0400

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,23 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
-* 4-new_dog.c - 
-* @param
-* @param
+* new_dog - creates a new dog struct
+* @name: first attrib
+* @age: second attrib
+* @owner: last struct attrib
 *
-* Decsription: 
+* Decsription: function that creates a new dog.
 * Return: Always(0) success
 */
 dog_t *new_dog(char *name, float age, char *owner);
 {
-	
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
