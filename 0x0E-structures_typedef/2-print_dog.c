87d2daf656abe9a1c05027de50677880accf25fc
Tigo-cmd <ja16162040@gmail.com>
2023-06-20 04:12:37 -0400

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,15 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner == "(nil)";
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner == "(nil)";
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %1F\n", d->age);
-	printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %1F\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }
