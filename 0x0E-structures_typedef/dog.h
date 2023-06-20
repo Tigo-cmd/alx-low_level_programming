94c2888c88abf4c4b7c010d01d2d98985653d5c8
Tigo-cmd <ja16162040@gmail.com>
2023-06-19 20:25:58 -0400

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - creates a structure named dog
+* @name: first attribute
+* @owner: second attribute
+* @age: last attribute
 
-struct dog {
-
+* Description: creates a dog like structure with name owner and the type
+*/
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
