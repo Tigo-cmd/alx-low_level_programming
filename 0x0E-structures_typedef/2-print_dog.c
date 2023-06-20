60dc301229d050f5185f844b3f95ca73e93c0f5f
Tigo-cmd <ja16162040@gmail.com>
2023-06-20 04:11:30 -0400

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,23 @@
-#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
 /**
-* 2-print_dog.c - 
-* @param
-* @param
+* print_dog - prints a struct dog
+* @d: pointer to attributes to be printed
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints a struct dog
+* Return: nothing
 */
-void print_dog(struct dog *d);
+void print_dog(struct dog *d)
 {
-	
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner == "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %1F\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
