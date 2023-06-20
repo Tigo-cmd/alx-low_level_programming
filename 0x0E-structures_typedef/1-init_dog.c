84ab5fa08903e3eda0e42a8cd53e8e32430a6356
Tigo-cmd <ja16162040@gmail.com>
2023-06-19 20:33:35 -0400

    updated

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 1-init_dog.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	
+} 
