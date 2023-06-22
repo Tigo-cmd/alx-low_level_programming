994378b69a444284534f3bc5cd09c7c15002afe9
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 19:45:00 -0400

    updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
 * Decsription: Function that prints a name
 * Return: nothing
 */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 		return;
