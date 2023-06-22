ca7e3df4442618d9e1769835dfcee18007a483b1
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 19:54:25 -0400

    updated

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+* 1-array_iterator.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	
+}
