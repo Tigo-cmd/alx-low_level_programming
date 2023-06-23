a60a6a35fc728f1d4b2d778a456608082008a4db
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 20:20:36 -0400

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,25 @@
 #include "function_pointers.h"
 /**
-* 1-array_iterator.c - 
-* @param
-* @param
+* array_iterator - executes a function given as a
+* parameter on each element of an array.
+* @array: array to be executed
+* @size: size of array
+* @action: pointer to function to be seen
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that executes a function
+* given as a parameter on each element of an array.
+* Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int));
 {
-	
+	int i = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
 }
