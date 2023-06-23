e7bc729829410acefce16df0551ac85e0b46b6a2
Tigo-cmd <ja16162040@gmail.com>
2023-06-23 04:19:26 -0400

    updated

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,7 @@
+#ifndef 3-CALC_H
+#define 3-CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /*3-calc_h*/
--- 0x0F-function_pointers/cfxn.sh
+++ 0x0F-function_pointers/cfxn.sh
@@ -2,7 +2,7 @@
 echo "file>"
 read file
 touch $file
-echo "#include \"function_pointers.h\"" > $file
+echo "#include \"3-calc.h\"" > $file
 echo "/**
 * $file - 
 * @param
@@ -18,10 +18,10 @@ echo "{
 	
 }" >> $file
 touch main0
-head -n -1 function_pointers.h > main0
+head -n -1 3-calc.h > main0
 echo $proto >> main0
 echo "#endif" >> main0
-mv main0 function_pointers.h
+mv main0 3-calc.h
 chmod 764 $file
 bash dirgit.sh
 gedit $file
