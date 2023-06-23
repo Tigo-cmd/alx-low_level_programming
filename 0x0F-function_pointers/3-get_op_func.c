6488f05a218632d4a5fc601d9736bf5f96c84cbf
Tigo-cmd <ja16162040@gmail.com>
2023-06-23 04:44:43 -0400

    updated

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+/**
+* 3-get_op_func.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+int (*get_op_func(char *s))(int, int);
+{
+	
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,62 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Description: A program 
-*
-* Return: 0(success)
-*/
+#include "3-calc.h"
 
-int main(void)
-{
-	return (0);		
-}
+/**
+ * op_add- returns the sum of to integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sum of the integers.
+ */
+ int op_add(int a, int b)
+ {
+ 	return (a + b);
+ }
+
+ /**
+ * op_add- returns the difference of a and b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: difference of the integers.
+ */
+ int op_sub(int a, int b)
+ {
+ 	return (a - b);
+ }
+
+ /**
+ * op_add- returns the product of a and b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: product of the integers.
+ */
+ int op_mul(int a, int b);
+ {
+ 	return (a * b);
+ }
+
+ /**
+ * op_add- returns the division of a by b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: division of the integers.
+ */
+ int op_div(int a, int b)
+ {
+ 	return (a / b);
+ }
+ 
+ /**
+ * op_add- returns the remainder of the divisior of a by b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: remainder of the division of the integers.
+ */
+ int op_mod(int a, int b)
+ {
+ 	return (a % b);
+ }
