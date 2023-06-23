230c176dc23fb16589142ee7166019a39daa0b92
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 20:23:24 -0400

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 * given as a parameter on each element of an array.
 * Return: nothing
 */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
