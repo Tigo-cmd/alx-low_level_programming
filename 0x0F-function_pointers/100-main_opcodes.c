f195e2aa9641ba3c560a1c3041d0ea8724d56207
Tigo-cmd <ja16162040@gmail.com>
2023-06-23 17:31:49 -0400

    updated

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * main - Entry point
+* @num: number of bytes
+* Description: A program that prints the opcodes of its own main function.
 *
-* Description: A program 
+* Return: 0(success)
+*/
+void main_opcode(int num);
+int main(int argc, char *argv[])
+{
+	int a;
+
+	argc = argc - 1; /*sets argc to 0 default*/
+	if (argc != 1)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	main_opcode(a);
+	return (0);
+}
+/**
+* main_opcode - gets and print out the opcode of main function
+* @num: number of bytes passed as argumment
 *
 * Return: 0(success)
 */
 
-int main(void)
+void main_opcode(int num)
 {
-	return (0);		
+	int i;
+	unsigned char *f = (unsigned char *)main;
+
+	while (i < num)
+	{
+		printf("%02x ", f[i]);
+		i++;
+	}
+	printf("\n");
+}
 }
