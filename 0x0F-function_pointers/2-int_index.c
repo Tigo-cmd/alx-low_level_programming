3be42797b426d043fa879ba3acdd390cb233e0ac
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 20:31:47 -0400

    updated

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+* 2-int_index.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	
+}
