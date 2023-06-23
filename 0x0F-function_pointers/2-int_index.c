69fb26c67fb1bba76983d46ec33747ad2a88a449
Tigo-cmd <ja16162040@gmail.com>
2023-06-22 21:15:27 -0400

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,30 @@
 #include "function_pointers.h"
 /**
-* 2-int_index.c - 
-* @param
-* @param
+* int_index -  returns the index of the first
+* element for which the cmp function does not return 0
+* @array: array to be searched
+* @size: no. of element in the array
+* @cmp: pointer to the function to be used to compare values
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that searches for an integer.
+* Return:  index of the first element
 */
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (cmp != NULL && array != NULL)
+	{
+		while (i < size)
+		{
+			cmp(array[i]);
+			i++;
+		}
+		if (cmp(array[i]) == 0)
+			return (-1);
+		else
+			return (array[0]);
+	}
 }
