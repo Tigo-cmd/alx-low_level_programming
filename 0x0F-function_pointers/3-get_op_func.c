e228e3b337592ca3ae526884056c158e3b674ddb
Tigo-cmd <ja16162040@gmail.com>
2023-06-23 07:10:25 -0400

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,31 @@
 #include "3-calc.h"
 /**
-* 3-get_op_func.c - 
-* @param
-* @param
+* get_op_func - selects the correct function to perform
+*                   the operation asked by the user
+* @s: operator passed as argument to the program
+* @get_op_func: pointer to op_function
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription:  function that selects the correct
+*           function to perform the operation asked by the user
+*		example get_op_func("-") == op_sub;
+* Return: pointer to the function
 */
-int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int)
 {
-	
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0, b = 5;
+
+	while (i < b)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+	}
+	return NULL; 
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,53 +2,53 @@
 #include "3-calc.h"
 
 /**
- * op_add- returns the sum of to integers
- * @a: first parameter
- * @b: second parameter
- *
- * Return: sum of the integers.
- */
- int op_add(int a, int b)
- {
- 	return (a + b);
- }
+* op_add- returns the sum of to integers
+* @a: first parameter
+* @b: second parameter
+*
+* Return: sum of the integers.
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
- /**
- * op_add- returns the difference of a and b
- * @a: first parameter
- * @b: second parameter
- *
- * Return: difference of the integers.
- */
- int op_sub(int a, int b)
- {
- 	return (a - b);
- }
+/**
+* op_add- returns the difference of a and b
+* @a: first parameter
+* @b: second parameter
+*
+* Return: difference of the integers.
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
- /**
- * op_add- returns the product of a and b
- * @a: first parameter
- * @b: second parameter
- *
- * Return: product of the integers.
- */
- int op_mul(int a, int b);
- {
- 	return (a * b);
- }
+/**
+* op_add- returns the product of a and b
+* @a: first parameter
+* @b: second parameter
+*
+* Return: product of the integers.
+*/
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
 
- /**
- * op_add- returns the division of a by b
- * @a: first parameter
- * @b: second parameter
- *
- * Return: division of the integers.
- */
- int op_div(int a, int b)
- {
- 	return (a / b);
- }
- 
+/**
+* op_add- returns the division of a by b
+* @a: first parameter
+* @b: second parameter
+*
+* Return: division of the integers.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
  /**
  * op_add- returns the remainder of the divisior of a by b
  * @a: first parameter
