a28cc2125f9aac7a7d5cc5034ce65a5ade357c7b
Tigo-cmd <ja16162040@gmail.com>
2023-06-24 13:41:04 -0400

    updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,13 +1,30 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
-* 1-print_numbers.c - 
-* @param
-* @param
+* print_numbers - prints numbers
+* @separator: string to be printed between numbers
+* @n: number of integers to be passed to the function
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints integers followed by a new line
+* Return: nothing
 */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	
+	va_list init;
+	int i = 0, b = n - 1;
+
+	va_start(init, n);
+	while (i < (int)n)
+	{
+		printf("%d", va_arg(init, int));
+
+		if (i < b && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		++i;
+	}
+	va_end(init);
+	printf("\n");
+
 }
