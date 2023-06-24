6924efe61e3c8dbd16361db4e68d9580be3f9c63
Tigo-cmd <ja16162040@gmail.com>
2023-06-24 13:05:06 -0400

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	int i = 0;
 
 	va_start(ap, n);
-	if (separator == NULL || n <= 0)
+	if (separator == NULL)
 		return;
 	while (i < (int)n)
 	{
