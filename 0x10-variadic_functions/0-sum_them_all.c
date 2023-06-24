cccbd0cef2d4be0c159a50f540c4df6e6c671584
Tigo-cmd <ja16162040@gmail.com>
2023-06-24 11:06:06 -0400

    updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,26 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
-* 0-sum_them_all.c - 
-* @param
-* @param
+* sum_them_all - sums all its parameters
+* @n: fixed parameter
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that returns the sum of all its parameters.
+* Return: the sum
 */
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
-	
+	va_list ap;
+	int i = 0, all = 0;
+
+	va_start(ap, n);
+	if (n == 0)
+		return (0);
+	while (i < (int)n)
+	{
+		all = all + va_arg(ap, int);
+		i++;
+	}
+
+	va_end(ap);
+	return (all);
 }
