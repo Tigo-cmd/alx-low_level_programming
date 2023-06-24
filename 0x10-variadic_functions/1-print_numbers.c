ce204de10277643b56895cf9d7d2b15fa1e1fc4a
Tigo-cmd <ja16162040@gmail.com>
2023-06-24 13:27:03 -0400

    updated

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+/**
+* 1-print_numbers.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	
+}
