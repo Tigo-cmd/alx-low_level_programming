e205a1f929ba0dc576617e06d039d50e2125b547
Tigo-cmd <ja16162040@gmail.com>
2023-06-24 12:55:29 -0400

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,13 +1,31 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
-* 2-print_strings.c - 
-* @param
-* @param
+* print_strings - prints strings.
+* @separator: string to be printed between strings
+* @n: number of strings passed to the function
 *
-* Decsription: 
-* Return: Always(0) success
+* Decsription: function that prints strings followed by a new line.
+* Return: nothing
 */
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
+	va_list ap;
+	int i = 0;
+
+	va_start(ap, n);
+	if (separator == NULL)
+		return;
+	while (i < (int)n)
+	{
+		char *ugo = va_arg(ap, char *);
+
+		if (ugo == NULL)
+			ugo = "(nil)";
+		if (i == i && i != 0)
+			printf("%s", separator);
+		printf("%s", ugo);
+		i++;
+	}
+	printf("\n");
 }
