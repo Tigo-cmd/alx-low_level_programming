849b3134d7a7c923611c1168021db7a8251455cb
Tigo-cmd <ja16162040@gmail.com>
2023-06-25 15:57:14 -0400

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_all(const char 0-sum_them_all.c 1-print_numbers.c 2-main.c 2-print_strings.c 3-print_all.c cfxn.sh dirgit.sh dirmain.sh filecreate.sh main0 README.md variadic_functions.h const format, ...);
+void print_all(const char * const format, ...);
 #endif
