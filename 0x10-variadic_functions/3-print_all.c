6b2983c0768faa8656fc99d91c90cc5bd33d087e
Tigo-cmd <ja16162040@gmail.com>
2023-06-25 14:44:16 -0400

    updated

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+/**
+* 3-print_all.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void print_all(const char 0-sum_them_all.c 1-print_numbers.c 2-main.c 2-print_strings.c 3-print_all.c cfxn.sh dirgit.sh dirmain.sh filecreate.sh README.md variadic_functions.h const format, ...);
+{
+	
+}
