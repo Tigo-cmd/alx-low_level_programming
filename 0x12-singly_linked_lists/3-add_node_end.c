a32b033d28a31ae3da2b638f80d24d778456c496
Tigo-cmd <ja16162040@gmail.com>
2023-07-04 18:54:49 -0400

    updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,38 @@
 #include "lists.h"
 /**
-* 3-add_node_end.c - 
-* @param
-* @param
+* add_node_end - inserts a node at the end of a list
+* @head: pointer to the first node
+* @str: string to be copied
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: address of the element or NULL if failed
 */
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
-	
+	unsigned int i = 0;
+
+	list_t *p = (list_t *)malloc(sizeof(list_t));
+
+	if (p == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	p->str = strdup((char *)str);
+	p->next = NULL;
+
+	list_t *end;
+
+	if (*head == NULL)
+	{
+		*head = p;
+	}
+	else
+	{
+		new = *head;
+		while (new->next != NULL)
+		{
+			new = new->next;
+		}
+		new->next = p;
+	}
+	return (p);
 }
