382a176b594041283bf45134f3ff173eb41c8958
Tigo-cmd <ja16162040@gmail.com>
2023-07-04 09:55:57 -0400

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,24 @@
 #include "list.h"
 /**
-* 0-print_list.c - 
-* @param
-* @param
-*
-* Decsription: 
-* Return: Always(0) success
+* print_list - prints all element of a list
+* @h: list to be printed
+* Return: number of nodes
 */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-	
+	list_t *begin = h;
+	size_t i = 0;
+
+	while (begin != NULL)
+	{
+		if (begin->str == NULL)
+		{
+			begin->str = "(nil)";
+			begin->len = 0;
+		}
+		printf("[%d] %s\n", begin->len, begin->str);
+		beg = beg->next;
+		i++;
+	}
+	return (i);
 }
