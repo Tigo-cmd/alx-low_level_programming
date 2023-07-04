57f4c07b4d327cc5ff27da1633c9523c6109a8d6
Tigo-cmd <ja16162040@gmail.com>
2023-07-04 12:59:11 -0400

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,22 @@
 #include "lists.h"
 /**
-* 2-add_node.c - 
-* @param
-* @param
+* add_node - adds a node to the begining of a list
+* @head: pointer to first node of the list
+* @str: string to be copied into head and printed
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: address of the new element of NULL if failed
 */
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *r = (list_t *)malloc(sizeof(list_t));
+
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	r->str = strdup((char *)str);
+	r->next = *head;
+	*head = r;
+
+	return (*head);
 }
