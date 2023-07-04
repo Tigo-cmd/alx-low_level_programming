a23db8a04d39ba7a4e3e8564b02f72fe3a2720d0
Tigo-cmd <ja16162040@gmail.com>
2023-07-04 14:05:45 -0400

    updated

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* 3-add_node_end.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+list_t *add_node_end(list_t **head, const char *str);
+{
+	
+}
