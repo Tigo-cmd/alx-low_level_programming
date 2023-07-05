c892e50fe81248d07d8a4e654fa3c247df646839
Tigo-cmd <ja16162040@gmail.com>
2023-07-05 12:18:01 -0400

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,34 @@
 #include "lists.h"
 /**
-* 3-add_nodeint_end.c - 
-* @param
-* @param
+* add_nodeint_end - adds a new node at the end of the listint_t list
+* @head: pointer to first node
+* @n: integer to be copied
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: Address of new element or null if failed
 */
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	
+	listint_t *p = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *q;
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->n = n;
+	p->next = NULL;
+
+	if (*head == NULL)
+		*head = p;
+	else
+	{
+		q = *head;
+		while (q->next != NULL)
+		{
+			q = q->next;
+		}
+		q->next = p;
+	}
+	return (p);
 }
