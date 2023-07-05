d469b22d17af8544bc3783862876c2f1eb54a1b6
Tigo-cmd <ja16162040@gmail.com>
2023-07-05 19:21:36 -0400

    updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,19 @@
 #include "lists.h"
 /**
-* 5-free_listint2.c - 
-* @param
-* @param
-*
-* Decsription: 
-* Return: Always(0) success
+* free_listint2 - frees a listint_t list
+* @head : pointer fo first node to be frred
+* Return: nothing
 */
-void free_listint2(listint_t **head);
+void free_listint2(listint_t **head)
 {
-	
+	listint_t *p = head;
+	listint_t *new;
+
+	while (p != NULL)
+	{
+		new = p->next;
+		free(p);
+		p = new;
+	}
+	head = NULL;
 }
