a913cbe1f56af123119b122a90f3d66361192b2c
Tigo-cmd <ja16162040@gmail.com>
2023-07-05 11:44:04 -0400

    updated

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,8 +12,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (p == NULL)
 		return (NULL);
-	p->n = (int *)n;
-	if (p->n == NULL)
+	p->n = n;
+	if (!p->n)
 	{
 		free(p);
 		return (NULL);
