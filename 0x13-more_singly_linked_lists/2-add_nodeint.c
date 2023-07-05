4ebe90f66f8594d34732505b07407271fa526ba6
Tigo-cmd <ja16162040@gmail.com>
2023-07-05 11:12:51 -0400

    updated

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* 2-add_nodeint.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+	
+}
