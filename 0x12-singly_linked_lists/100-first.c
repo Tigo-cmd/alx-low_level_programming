ff40ea853d3e2533ff4a820be4736de63e094e6c
Tigo-cmd <ja16162040@gmail.com>
2023-07-05 18:13:14 -0400

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* first - Entry point
 *
-* Description: A program 
+* Description: A program prints
 *
 * Return: 0(success)
 */
 
-int main(void)
+void __attribute__((constructor)) first(void)
 {
-	return (0);		
+	printf("You're beat! and yet, ");
+	printf("you must allow,\nI bore my house upon my back!\n");
 }
