c1d3e1dcc939565862eac5190e60638908fbd138
Tigo-cmd <ja16162040@gmail.com>
2023-07-06 19:28:02 -0400

    hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,31 @@
 #include "lists.h"
 /**
-* 9-insert_nodeint.c - 
-* @param
-* @param
+* insert_nodeint_at_index -  function that inserts a new node
+*				at a given position.
 *
-* Decsription: 
+* @head: pointer to the first node of the lists
+* @idx: index of the list where the new node should be added
+* @n: data to be added at the given position
+*
 * Return: Always(0) success
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	
+	listint_t *p = *head;
+	listint_t *b = (listint_t *)malloc(sizeof(listint_t));
+	int current = 0;
+
+	if (p == NULL)
+		return (NULL);
+	if (b == NULL)
+		return (NULL);
+	while (current < (int)idx - 1)
+	{
+		p = p->next;
+		current++;
+	}
+	b->n = n;
+	b->next = p->next;
+	p->next = b;
+	return (b);
 }
