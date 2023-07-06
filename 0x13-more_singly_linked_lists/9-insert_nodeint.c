50e0218d896548296968e2468872d258f4106d37
Tigo-cmd <ja16162040@gmail.com>
2023-07-06 16:18:03 -0400

    hot fix

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* 9-insert_nodeint.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+{
+	
+}
