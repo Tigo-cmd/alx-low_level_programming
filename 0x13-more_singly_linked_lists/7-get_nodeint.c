19b48e762370321da111c2c237035d685bd79080
Tigo-cmd <ja16162040@gmail.com>
2023-07-06 10:31:25 -0400

    updated

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* 7-get_nodeint.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+{
+	
+}
