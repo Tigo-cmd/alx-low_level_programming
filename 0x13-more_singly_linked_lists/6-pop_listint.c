7481165eddddf05723831767fea0d34ef8a0a51e
Tigo-cmd <ja16162040@gmail.com>
2023-07-06 06:11:21 -0400

    updated

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
 * pop_listint - eletes the head node of a listint_t linked list
 * @head: pointer to head node to be deleted
@@ -7,9 +8,12 @@
 */
 int pop_listint(listint_t **head)
 {
+	if (*head == NULL)
+		return (0);
 	listint_t *p = *head;
 	if (p == NULL)
 		return (0);
 	*head = (*head)->next;
+	free(p);
 	return (p->n);
 }
