81768590aa245268af6b0b5da1eaee54e14ce8e0
Tigo-cmd <ja16162040@gmail.com>
2023-07-06 10:51:11 -0400

    updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,25 @@
 #include "lists.h"
 /**
-* 7-get_nodeint.c - 
-* @param
-* @param
+* get_nodeint_at_index - searches for a given node position
+* and returns the data of the node
+* @head: pointer the first node data
+* @index: position is the node in the list
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: nth/data node of the list listint_t
 */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	
+	listint_t *p = head;
+	int no = 0;
+
+	while (n < (int)index)
+	{
+		if (!p)
+			return (NULL);
+		p = p->next;
+		no++;
+	}
+	if (p == NULL)
+		return (NULL);
+	return (p);
 }
