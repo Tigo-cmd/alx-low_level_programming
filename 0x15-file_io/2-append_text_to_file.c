baac177235691992a295a6337334bbe2f8691771
Tigo-cmd <ja16162040@gmail.com>
2023-07-11 11:42:00 -0400

    updated

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 2-append_text_to_file.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+int append_text_to_file(const char *filename, char *text_content);
+{
+	
+}
