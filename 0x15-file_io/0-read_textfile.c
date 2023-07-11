4902ddb6a9277240718709d2c3102a45e2d8251f
Tigo-cmd <ja16162040@gmail.com>
2023-07-10 22:27:45 -0400

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,37 @@
 #include "main.h"
 /**
-* 0-read_textfile.c - 
-* @param
-* @param
+* read_textfile -  reads a text file and prints it
+* to the POSIX standard output.
+* @filename: pointer to file to be copied
+* @letters: number of letters it should print
 *
-* Decsription: 
-* Return: Always(0) success
+* Return:  actual number of letters that can be read and printed
 */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	
+	int file;
+	char buff[letters + 1];
+	ssize_t rd, wr;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	rd = read(file, buff, letters);
+	if (rd <= 0)
+	{
+		close(file);
+		return (0);
+	}
+	buff[rd] = '\0';
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr != rd || wr < 0)
+	{
+		close(file);
+		return (0);
+	}
+
+	close(file);
+	return (rd);
 }
