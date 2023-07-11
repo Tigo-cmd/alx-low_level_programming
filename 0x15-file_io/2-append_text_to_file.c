1f4c9bec9c06ca41e358b33436f5dcde9dfba05c
Tigo-cmd <ja16162040@gmail.com>
2023-07-11 11:56:46 -0400

    updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,29 @@
 #include "main.h"
 /**
-* 2-append_text_to_file.c - 
-* @param
-* @param
+* append_text_to_file - appends text at the end of a file.
+* @filename: pointer to file to be used
+* @text_content: content to add at the end of the file
 *
-* Decsription: 
 * Return: Always(0) success
 */
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int wr, rd, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	rd = open(filename, O_WRONLY | O_APPEND);
+	if (rd <= 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		wr = write(rd, text_content, i);
+		if (wr <= 0)
+			return (-1);
+	}
+
+	close(rd);
+	return (1);
 }
