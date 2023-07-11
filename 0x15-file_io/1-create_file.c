312356c28eb0b50d9fa2ee6b089abb7c5be29357
Tigo-cmd <ja16162040@gmail.com>
2023-07-11 00:01:46 -0400

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,29 @@
 #include "main.h"
 /**
-* 1-create_file.c - 
-* @param
-* @param
+* create_file - function that creates a file.
+* @filename: pointer to the file
+* @text_content: content of the file
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: 1 on success, -1 on failure
 */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	
+	int file, wr, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	rd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (rd <= 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		wr = write(file, text_content, i);
+		if (wr <= 0)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
 }
