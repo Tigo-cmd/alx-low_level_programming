111f4e4c7784226faa9e3252a8521a8f47a0046f
Tigo-cmd <ja16162040@gmail.com>
2023-07-10 20:05:08 -0400

    updated

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 0-read_textfile.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	
+}
