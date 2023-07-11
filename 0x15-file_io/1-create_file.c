b7508a32fed5be732717c59e14fcbbea251351f1
Tigo-cmd <ja16162040@gmail.com>
2023-07-10 23:17:28 -0400

    updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 1-create_file.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+int create_file(const char *filename, char *text_content);
+{
+	
+}
