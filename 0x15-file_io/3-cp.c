c917b277748ca6fa06ed063227acbd5343a9f7aa
Tigo-cmd <ja16162040@gmail.com>
2023-07-14 06:55:04 -0400

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@
 int main(int ac, char **av)
 {
 	ssize_t rd, wr, rd2;
-	char *buff;
+	char buff[1024];
 	int file;
 	/* sets the ac to 0 for easy implementation */
 	ac = ac - 1;
@@ -21,34 +21,32 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = (char *)malloc(sizeof(char *) * 1024);
-	if (buff == NULL)
-	{
-		free(buff);
-		return (-1);
-	}
 	file = open(av[1], O_RDONLY);
-	rd2 = read(file, buff, 1024);
-	if (av[1] == NULL || rd2 <= 0 || file == -1)
+	if (file == -1)
 	{
-		close(file);
 		dprintf(2, "Error: can't read file from file %s\n", av[1]);
 		exit(98);
 	}
-	rd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	wr = write(rd, buff, 1024);
-	if (wr <= 0)
+	rd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((rd2 = read(file, buff, 1024)) != 0)
 	{
-		dprintf(2, "Error: can't write to %s\n", av[2]);
-		exit(99);
+		wr = write(rd, buff, rd2);
+		if (wr == -1 || rd == -1)
+		{
+			dprintf(2, "Error: can't write to %s\n", av[2]);
+			close(file);
+			exit(99);
+		}
 	}
-	if (!close(file))
+	if (rd2 == -1)
+	{
+		dprintf(2, "Error: can't read file from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(file) == -1)
 	{
 		dprintf(2, "Error: can't close %d\n", file);
 		exit(100);
 	}
-	free(buff);
-	close(file);
-	close(rd);
 	return (0);
 }
