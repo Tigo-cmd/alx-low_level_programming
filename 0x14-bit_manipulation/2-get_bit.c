2137fe94b0d98f13c11e1119bc31f8510888e604
Tigo-cmd <ja16162040@gmail.com>
2023-07-16 19:26:20 -0400

    updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-* 2-get_bit.c - 
-* @param
-* @param
+* get_bit - returns the value of the bit of a given index
+* @n: number to search
+* @index: index to search
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: value of bit at given index or -1 of failed
 */
 int get_bit(unsigned long int n, unsigned int index);
 {
-	
+	int value;
+
+	if (index > 63)
+		return (-1);
+
+	value = (n >> index) & 1;
+
+	return (value);
 }
