9b07526df8e42af300cc497a40d8c6ea832bb73e
Tigo-cmd <ja16162040@gmail.com>
2023-07-16 20:14:51 -0400

    updated

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-* 3-set_bit.c - 
-* @param
-* @param
+* set_bit - function that sets the value of a bit to 1 at a given index.
+* @n: number to be set
+* @index: index is the index, starting from 0 of the bit you want to set
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: 1 if it worked, or -1 if an error occurred
 */
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int value;
+
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	value = 1UL << index;
+	*n = *n | value;
+	return (1);
 }
