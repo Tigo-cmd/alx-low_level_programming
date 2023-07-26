1cc3eb3dbc698087ff41590d6dbd88810aab94dc
Tigo-cmd <ja16162040@gmail.com>
2023-07-26 19:09:00 -0400

    updated

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
-* 100-get_endianness.c - 
-* @param
-* @param
+* get_endianness - CHECKS for endianness
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: 0 for big endia or 1 for small endain
 */
-int get_endianness(void);
+int get_endianness(void)
 {
-	
+	int end = 1;
+	char *main_word = (char *) &end;
+
+	if (*main_word != 1)
+		return (0);
+	else
+		return (1);
 }
