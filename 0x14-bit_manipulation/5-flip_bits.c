be6e2c5ff60f51d60186be9bf945b83b1c1c781b
Tigo-cmd <ja16162040@gmail.com>
2023-07-26 18:56:37 -0400

    updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 5-flip_bits.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+{
+	
+}
