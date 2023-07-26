e7f55d7231892d2031f4ade50899576087a5951f
Tigo-cmd <ja16162040@gmail.com>
2023-07-26 18:44:17 -0400

    updated

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* 4-clear_bit.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+	
+}
