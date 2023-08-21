834698e4b8499c49c901e729b56582e2c5af96be
Tigo-cmd <ja16162040@gmail.com>
2023-08-21 17:16:10 -0400

    Giga Millions program

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bib/bash
-wget -P ..https://github.com/Tigo-cmd/alx-low_level_programming/blob/main/0x18-dynamic_libraries/tigo.so
+wget -P .. https://github.com/Tigo-cmd/alx-low_level_programming/raw/main/0x18-dynamic_libraries/tigo.so
 export LD_PRELOAD="$PWD/../tigo.so"
