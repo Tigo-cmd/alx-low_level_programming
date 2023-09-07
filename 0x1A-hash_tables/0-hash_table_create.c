cb43714a7c59442bed040c66e7079725cf04df3d
Tigo-cmd <ja16162040@gmail.com>
2023-09-07 15:05:34 -0400

    function that creates a hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+/**
+* 0-hash_table_create.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+hash_table_t *hash_table_create(unsigned long int size);
+{
+	
+}
