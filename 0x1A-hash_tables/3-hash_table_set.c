6b19f3f109c5f94a9a13bdfd239122b764bd1dd9
Tigo-cmd <ja16162040@gmail.com>
2023-09-07 15:50:18 -0400

    function that retrieves a value associated with a key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,12 @@
 #include "hash_tables.h"
 /**
-* 3-hash_table_set.c - 
-* @param
-* @param
+* hash_table_set - adds element to the hash table
+* @set: a non empty string
+* @value:  key value pair
 *
-* Decsription: 
 * Return: Always(0) success
 */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	
 } 
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+* 4-hash_table_get.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key);
+{
+	
+} 
