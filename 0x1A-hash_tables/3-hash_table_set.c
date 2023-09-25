0b835ca5c213c6e47bc40188307b5ae7c7d0bf4f
Tigo-cmd <ja16162040@gmail.com>
2023-09-25 14:08:36 -0400

    function that retrieves a value associated with a key.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,42 +8,50 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_dup;
-	unsigned long int index, i;
+	unsigned long int i, x;
+	hash_node_t *new_node;
+	char *dup_value
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL)
+	{
 		return (0);
-
-	value_dup = strdup(value);
-	if (!value_dup)
+	}
+	if (key == NULL)
+	{
 		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	}
+	if (value == NULL)
+	{
+	return (0);
+	}
+	dup_value = strdup(value);
+	if (dup_value == NULL)
+		return (0);
+	x = key_index((const unsigned char *)key, ht->size);
+	for (i = x; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_dup;
+			ht->array[i]->value = dup_value;
 			return (1);
 		}
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 	{
-		free(value_dup);
+		free(dup_value);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (0);
 	}
-	new->value = value_dup;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
+	new_node->value = dup_value;
+	new_node->next = ht->array[x];
+	ht->array[x] = new_node;
+	
 	return (1);
-}
+} 
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,32 @@
 #include "hash_tables.h"
 /**
-* hash_table_get - 
-* @key: string non-empty
-*
+* hash_table_get - function that retrieves a value associated with a key.
+* @key: the key you are looking for
+* @ht: the hash table you want to look into
 * Return: Always(0) success
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *new_node;
+	unsigned long int index;
 	
-} 
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+	{
+		return (NULL);
+	}
+	new_node = ht->array[index];
+	while (new_node && strcmp(new_node->key, key) != 0)
+	{
+		new_node = new_node->next;
+	}
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	return (new_node->value);
+}
