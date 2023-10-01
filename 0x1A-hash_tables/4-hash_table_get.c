3c60fd27131e1fb0e625209c2835f85970b2f8c5
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 18:34:43 -0400

    debugging

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *new_node;
 	unsigned long int index;
-	
+
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		return (NULL);
