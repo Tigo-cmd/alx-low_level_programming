a3eeb07f84a91ab51890088c4cc57b9bb942f952
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 18:47:04 -0400

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+* 5-hash_table_print.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void hash_table_print(const hash_table_t *ht);
+{
+	
+}
