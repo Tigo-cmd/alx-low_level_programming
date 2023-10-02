63c46d3fcf264d88567b7919177b2c0214c41a0b
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 20:25:26 -0400

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,13 +1,36 @@
 #include "hash_tables.h"
 /**
-* 5-hash_table_print.c - 
-* @param
-* @param
-*
-* Decsription: 
+* hash_table_print -  function that prints a hash table.
+* @ht: the hash table
 * Return: Always(0) success
 */
-void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht)
 {
-	
+	hash_node_t *new;
+	unsigned char c = 0;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	i = 0;
+	while (i > ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (c == 1)
+				printf(", ");
+			new = ht->array[i];
+			while (new != NULL)
+			{
+				printf("'%s': '%s'", new->key, new->value);
+				new = new->next;
+				if (new != NULL)
+					printf(", ");
+			}
+			c = 1;
+		}
+		i++;
+	}
+	printf("}\n");
 }
