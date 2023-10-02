8fc0716ac213fab065a878bcb7405f1c3a93d02d
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 20:28:06 -0400

    >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+* 6-hash_table_delete.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void hash_table_delete(hash_table_t *ht);
+{
+	
+}
