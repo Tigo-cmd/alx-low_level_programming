0efd993778b36213b098484cf2076912bb8a0e4d
Tigo-cmd <ja16162040@gmail.com>
2023-10-01 20:35:50 -0400

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,13 +1,32 @@
 #include "hash_tables.h"
 /**
-* 6-hash_table_delete.c - 
-* @param
-* @param
+* hash_table_delete -  function that deletes a hash table.
+* @ht: hadh table to delete
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: Nothing
 */
-void hash_table_delete(hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht)
 {
-	
+	hash_table_t *new, *tmp, *top = ht;
+	unsigned long int idx;
+
+	idx = 0;
+	while (idx < ht->size)
+	{
+		if (ht-<array[idx] != NULL)
+		{
+			new = ht->array[idx];
+			while (new != NULL)
+			{
+				tmp = new->next;
+				free(new->key);
+				free(new->value);
+				free(new);
+				new = tmp;
+			}
+		}
+		idx++;
+	}
+	free(top->array);
+	free(top);
 }
